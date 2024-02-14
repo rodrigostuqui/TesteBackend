1bc0a1888376ca4e9c21d0279fbc283c0d23f4a1
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2024-02-14 15:57:56 -0300

    feat(headcount): adiciona pesquisa por periodo e por categoria

--- headcount/models.py
+++ headcount/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import Count
+from datetime import datetime
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 class Headcount(models.Model):
     id_employee = models.CharField(max_length=300, null=False)
@@ -10,3 +13,15 @@ class Headcount(models.Model):
     fg_status = models.IntegerField()
     fg_dismissal_on_month = models.IntegerField()
     dt_reference_month = models.DateField()
+
+    @classmethod
+    def count_employees_by_period(cls, init_date : datetime, end_date : datetime):
+        employee_counts = cls.objects.filter(dt_reference_month__range=[init_date, end_date], fg_status=True)
+        employee_counts = employee_counts.values(year=ExtractYear('dt_reference_month'), month=ExtractMonth('dt_reference_month'))
+        return employee_counts.annotate(count = Count('id'))
+    
+    @classmethod
+    def count_employees_by_category(cls, init_date : datetime, end_date : datetime, category : str):
+        employee_counts = cls.objects.filter(dt_reference_month__month=end_date.month, dt_reference_month__year=end_date.year, fg_status=True, ds_category_5=category)
+        return employee_counts.values('ds_category_4').annotate(count = Count('id'))
+        
--- headcount/views.py
+++ headcount/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Headcount
+from datetime import datetime
+from django.http import JsonResponse
+from utils import DataUtils
+def headcount_linechart(request):
+    if request.method == 'GET':
+        init_date = request.GET.get('init_date')
+        end_date = request.GET.get('end_date')
+        init_date = datetime.strptime(init_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        employee_counts = Headcount.count_employees_by_period(init_date=init_date, end_date=end_date)
+        data = DataUtils.transform_query_in_json_linechart(employee_counts)
+        return JsonResponse(data, safe=False)
 
-# Create your views here.
+
+def headcount_categorychart(request):
+    if request.method == 'GET':
+        init_date = request.GET.get('init_date')
+        end_date = request.GET.get('end_date')
+        category = request.GET.get('category')
+        init_date = datetime.strptime(init_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        employee_counts = Headcount.count_employees_by_category(init_date, end_date, category)
+        data = DataUtils.transform_query_in_json_categorychart(employee_counts)
+        return JsonResponse(data, safe=False)
+
+
