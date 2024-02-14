2f7c2610d2490ad87a0333efbfe801a3e22b749d
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2024-02-14 15:58:26 -0300

    feat(turnover): adiciona pesquisa por periodo e categoria

--- turnover/models.py
+++ turnover/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Count, FloatField, Func
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 class Turnover(models.Model):
     id_employee = models.CharField(max_length=300, null=False)
@@ -10,3 +12,31 @@ class Turnover(models.Model):
     fg_status = models.IntegerField()
     fg_dismissal_on_month = models.IntegerField()
     dt_reference_month = models.DateField()
+
+    @classmethod
+    def get_mean_active_employees_on_period(cls, init_date, end_date):
+        month_diference = end_date - init_date
+        month_diference = month_diference.days/30
+        employee_count = cls.objects.filter(dt_reference_month__range=[init_date, end_date], fg_status=True).count()
+        return employee_count/month_diference
+    
+    @classmethod
+    def get_turnover_linechart(cls, init_date, end_date):
+        employee_dismissal = cls.objects.filter(dt_reference_month__range=[init_date, end_date], fg_dismissal_on_month=True)
+        employee_dismissal = employee_dismissal.values(year=ExtractYear('dt_reference_month'), month=ExtractMonth('dt_reference_month'))
+        mean_active_employees = cls.get_mean_active_employees_on_period(init_date, end_date)
+        return employee_dismissal.annotate(count=Func(
+        Count('id') / float(mean_active_employees) * 100,
+        function='ROUND',
+        template='%(function)s(%(expressions)s, 2)',
+        output_field=FloatField(),))
+    
+    @classmethod
+    def get_turnover_categorychart(cls, init_date, end_date, category):
+        active_employees = cls.objects.filter(dt_reference_month__month=end_date.month, dt_reference_month__year=end_date.year, fg_status=True, ds_category_5=category).count()
+        employees_dismissal = cls.objects.filter(dt_reference_month__month=end_date.month, dt_reference_month__year=end_date.year, fg_dismissal_on_month=True, ds_category_5=category)
+        return employees_dismissal.values('ds_category_4').annotate(count=Func(
+        Count('id') / float(active_employees) * 100,
+        function='ROUND',
+        template='%(function)s(%(expressions)s, 2)',
+        output_field=FloatField(),))
--- turnover/views.py
+++ turnover/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from datetime import datetime
+from .models import Turnover
+from django.http import JsonResponse
+from utils import DataUtils
 # Create your views here.
+def turnover_linechart(request):
+    if request.method == 'GET':
+        init_date = request.GET.get('init_date')
+        end_date = request.GET.get('end_date')
+        init_date = datetime.strptime(init_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        employee_dimissal = Turnover.get_turnover_linechart(init_date, end_date)
+        data = DataUtils.transform_query_in_json_linechart(employee_dimissal)
+        return JsonResponse(data, safe=False)
+
+def turnover_categorychart(request):
+    if request.method == 'GET':
+        init_date = request.GET.get('init_date')
+        end_date = request.GET.get('end_date')
+        category = request.GET.get('category')
+        init_date = datetime.strptime(init_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        employee_dimissal = Turnover.get_turnover_categorychart(init_date, end_date, category)
+        data = DataUtils.transform_query_in_json_categorychart(employee_dimissal)
+        return JsonResponse(data, safe=False)
